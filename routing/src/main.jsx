27d3327de0a85117b46d9a454be48353ae5b033d
angytobio <142904995+fewfewtobio@users.noreply.github.com>
2024-08-17 16:42:16 +0700

    edit main to hash

--- routing/src/main.jsx
+++ routing/src/main.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+import {createHashRouter,RouterProvider,} from "react-router-dom";
 import "./index.css";
 import ErrorPage from "./error-page";
 import Root, {loader as rootLoader,action as rootAction,} from "./routes/root";
@@ -9,7 +9,7 @@ import EditContact, {action as editAction,} from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
 import Index from "./routes/index";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Root />,
